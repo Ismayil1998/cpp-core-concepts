d5561eb8ffbeb7866f80005aeeabf44f4dfd3619
Ismayil1998 <ismayilov1998@gmail.com>
2026-01-06 15:45:28 +0400

    Update SimpleProblemsWithNumbers.cpp

--- algorithms/SimpleProblemsWithNumbers.cpp
+++ algorithms/SimpleProblemsWithNumbers.cpp
@@ -62,4 +62,93 @@ int main(){
    {
     cout<<"Wrong digit"<<endl;
    }
+
+   //Find the sum and product of its digits.
+   int number;
+   cin>>number;
+   if (number>10&&number<100)
+   {
+    int divid=number/10;
+    int modul=number%10;
+    int sum=divid+modul;
+    int product=divid*modul;
+    cout<<"Sum of the digits: "<<sum<<endl;
+    cout<<"Product of the digits: "<<product<<endl;
+   }
+   else
+   {
+    cout<<"Wrong digit!"<<endl;
+   }  
+
+   //Print the number obtained by rearranging the digits of the original number.
+   int number;
+   cin>>number;
+   if (number>10&&number<99)
+   {
+    int divid=number/10;
+    int modul=number%10;
+    if (modul==0)
+    {
+        
+        cout<<divid<<endl;
+    }else{
+        cout<<modul<<divid<<endl;
+    }
+   }else{
+    cout<<"Wrong digit!"<<endl;
+   }
+
+   //Using one division operation entirely, output the first digit of this number (hundreds).
+   int number;
+   cin>>number;
+   if (number>100&&number<999)
+   {
+    int division=number/100;
+    cout<<division<<endl;
+   }else{
+    cout<<"Wrong digit!"<<endl;
+   }   
+
+   //Print first its last digit (units), and then its middle digit (tens).
+   int number;
+   cin>>number;
+  if (number>100&&number<999)
+  {
+    int modul=number%10;
+    cout<<"Last digit (units): "<<modul<<endl;
+    int division=(number/10)%10;
+    cout<<"Middle digit (tens): "<<division<<endl;
+
+  }else{
+    cout<<"Wrong digit!"<<endl;
+  }
+
+  //Find the sum and product of its digits.
+  int number;
+  cin>>number;
+  if (number>100&&number<999)
+  {
+    int modul=number%10;
+    int modul1=(number/10)%10;
+    int modul2=number/100;
+    int total=modul+modul1+modul2;
+    int product=modul*modul1*modul2;
+    cout<<"Sum of the digits: "<<total<<endl;
+    cout<<"Product of the digits: "<<product<<endl;
+  }else{
+    cout<<"Wrong digit !"<<endl;
+  }  
+
+  //Reverse three-digit integer
+  int number;
+  cin>>number;
+  if (number>100&&number<999)
+  {
+    int modul=number%10;
+    int modul1=(number/10)%10;
+    int modul2=number/100;
+    cout<<modul<<modul1<<modul2;
+  }else{
+    cout<<"Wrong digit!"<<endl;
+  }  
 }
